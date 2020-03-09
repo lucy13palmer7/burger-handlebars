4646bd66490afabf54fa63fdfc82d9fe7f2c8dc5
Lucy <lucy13palmer7@gmail.com>
2020-03-09 17:33:56 -0600

    adding routes, handlebars files, and server.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
 const mysql = require("mysql");
-const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+var connection;
 
-// CREATING MY CONNECTION TO SQL DATABASE
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burgerDB"
-});
+if (process.env.BURGERSDB_URL) {
+  connection = mysql.createConnection(process.env.BURGERSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgersDB"
+  });
+}
 
-// CONNECT TO THE MYSQL SERVER AND SQL DATABASE
 connection.connect(function(err) {
-  if (err) throw err;
-  startApp();
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
+
 module.exports = connection;
