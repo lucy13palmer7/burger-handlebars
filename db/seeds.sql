a71df54c10d3c1373e6e08ca4aeaedf002256749
Lucy <lucy13palmer7@gmail.com>
2020-03-11 21:23:33 -0600

    fixed seeds file, and wishlist is working. Need to style with bootstrap some more

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-INSERT INTO burgers (name) VALUES ('Buffalo Burger')
+INSERT INTO burgers (name) VALUES ('Buffalo Burger');
+INSERT INTO burgers (name) VALUES ('American Classic Cheese Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Veggitarian Black Bean Burger', true);
